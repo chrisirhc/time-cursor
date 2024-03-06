6dea03dfe9a31a36c4f48b5a1b9e23aa7c16b563
Chris Chua <chris.sirhc@gmail.com>
2024-03-06 21:23:28 +0800

    add initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TimeCursor from './components/TimeCursor.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Time Cursor</h1>
+  </header>
+
+  <main>
+    <TimeCursor />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TimeCursor.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const text = 'Hello, Vue 3!';
+</script>
+
+<template>
+  <textarea v-model="text"></textarea>
+</template>
+
+<style scoped>
+textarea {
+  width: 500px;
+  height: 500px;
+}
+</style>
