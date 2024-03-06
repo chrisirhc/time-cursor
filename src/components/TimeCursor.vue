9dcaf26ebd7738def9a6bb093a27e9d5fc6c984c
Chris Chua <chris.sirhc@gmail.com>
2024-03-06 22:30:17 +0800

    test out time cursor logic

--- src/components/TimeCursor.vue
+++ src/components/TimeCursor.vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
-const text = 'Hello, Vue 3!';
+import { ref, onMounted, onUpdated, onUnmounted } from 'vue'
+
+// declare a ref to hold the element reference
+// the name must match template ref value
+const input = ref<HTMLInputElement | null>(null)
+let text = defineModel<string>();
+let lastSelection: null | number = null;
+let intervalId: number;
+
+onMounted(() => {
+  intervalId = setInterval(() => {
+    const inputElement = input.value!;
+    const selectionStart = inputElement.selectionStart;
+    // Find the cursor position, and append times to line ends.
+    if (selectionStart) {
+      const textValue = text.value!;
+      const textBefore = textValue.substring(0, selectionStart);
+      const textAfter = textValue.substring(selectionStart);
+
+      text.value = textBefore + new Date().toLocaleTimeString() + textAfter;
+      lastSelection = selectionStart;
+    }
+  }, 1000)
+});
+
+onUpdated(() => {
+  if (lastSelection === null) return;
+    const inputElement = input.value!;
+  inputElement.setSelectionRange(lastSelection, lastSelection);
+  lastSelection = null;
+});
+
+onUnmounted(() => {
+  clearInterval(intervalId);
+});
+
 </script>
 
 <template>
-  <textarea v-model="text"></textarea>
+  <textarea ref="input" v-model="text"></textarea>
 </template>
 
 <style scoped>
